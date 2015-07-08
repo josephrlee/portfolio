bb42ea2385e9385ba968b8428a2baf55916892ea
Joseph Lee <josephrlee@gmail.com>
2015-07-08 01:09:14 -0700

    Fixed smooth scrolling to work with sticky kit and nav overlay
    -Customized hamburger menu to fit site
    -Fixed body, html height bug that causes sticky kit to break
    -Formated styles
    -Created site.js file for site

---
+++ scripts/site.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  // Full Screen Nav Overlay
+  $('.menu-burger, .menu-items').on('click', function() {
+    $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
+    $('.menu-burger').text() == "☰" ? $('.menu-burger').text('✕') : $('.menu-burger').text('☰');
+  });
+
+  // Sticky Kit
+  $(".info").stick_in_parent();
+
+  // Smooth Scrolling
+  $('a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top
+    }, 900, 'swing', function() {
+      window.location.hash = target;
+    });
+  });
+});
